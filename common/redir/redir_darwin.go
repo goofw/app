8d3997259dda8e9d7489dd078f0986c4fc27df43
goofw <59505666+goofw@users.noreply.github.com>
2022-12-30 18:20:59 +0800

    Create redir_darwin.go

---
+++ common/redir/redir_darwin.go
@@ -0,0 +1,37 @@
+package redir
+
+import (
+    "fmt"
+    "os/exec"
+    "strconv"
+    "strings"
+    "net"
+    "net/netip"
+)
+
+func GetOriginalDestination(conn net.Conn) (destination netip.AddrPort, err error) {
+    addr, _ := lookup(conn.RemoteAddr().(*net.TCPAddr))
+    dest := addr.AddrPort()
+    return dest, nil
+}
+
+func lookup(addr *net.TCPAddr) (*net.TCPAddr, error) {
+    var new_addr net.TCPAddr
+    
+    out, _ := exec.Command("sudo", "-n", "/sbin/pfctl", "-s", "state").Output()
+    for _, line := range strings.Split(string(out), "\n") {
+        if strings.Contains(line, "ESTABLISHED:ESTABLISHED") {
+            if strings.Contains(line, fmt.Sprintf("%s:%d", addr.IP.String(), addr.Port)) {
+                fields := strings.Fields(line)
+                if len(fields) > 4 {
+                    addr := strings.Split(fields[4], ":")
+                    new_addr.IP = net.ParseIP(addr[0])
+                    new_addr.Port, _ = strconv.Atoi(addr[1])
+                    break
+                }
+            }
+        }
+    }
+
+    return &new_addr, nil
+}
