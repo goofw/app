982233c378c446516ddacf3ab0b30fb326227dd0
goofw <59505666+goofw@users.noreply.github.com>
2024-03-24 20:15:46 +0800

    Update redir_darwin.go

--- common/redir/redir_darwin.go
+++ common/redir/redir_darwin.go
@@ -10,15 +10,17 @@ import (
 )
 
 func GetOriginalDestination(conn net.Conn) (destination netip.AddrPort, err error) {
-    addr, _ := lookup(conn.RemoteAddr().(*net.TCPAddr))
-    dest := addr.AddrPort()
-    return dest, nil
+    addr, err := lookup(conn.RemoteAddr().(*net.TCPAddr))
+    if err != nil {
+		return netip.AddrPort{}, err
+	}
+    return addr.AddrPort(), nil
 }
 
 func lookup(addr *net.TCPAddr) (*net.TCPAddr, error) {
     var new_addr net.TCPAddr
     
-    out, _ := exec.Command("sudo", "-n", "/sbin/pfctl", "-s", "state").Output()
+    out, err := exec.Command("sudo", "-n", "/sbin/pfctl", "-s", "state").Output()
     for _, line := range strings.Split(string(out), "\n") {
         if strings.Contains(line, "ESTABLISHED:ESTABLISHED") {
             if strings.Contains(line, fmt.Sprintf("%s:%d", addr.IP.String(), addr.Port)) {
@@ -33,5 +35,5 @@ func lookup(addr *net.TCPAddr) (*net.TCPAddr, error) {
         }
     }
 
-    return &new_addr, nil
+    return &new_addr, err
 }
